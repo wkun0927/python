07092117a4726799aca3c370dd2829b1f0e2fd61
wangkun <wkun0927@gmail.com>
2021-11-08 16:51:25 +0800

    图像处理

--- 工作内容/专利/get_cookie.py
+++ 工作内容/专利/get_cookie.py
@@ -54,27 +54,27 @@ def getDriver():
 
 
 def recognize_text(image):
-        # 打开文件夹中的图片
-        image = Image.open('./img/cut.png')
-        # 灰度图
-        lim = image.convert('L')
-        # 灰度阈值设为165，低于这个值的点全部填白色
-        threshold = 150
-        table = []
+    # 打开文件夹中的图片
+    image = Image.open('./img/cut.png')
+    # 灰度图
+    lim = image.convert('L')
+    # 灰度阈值设为165，低于这个值的点全部填白色
+    threshold = 150
+    table = []
 
-        for j in range(256):
-            if j < threshold:
-                table.append(0)
-            else:
-                table.append(1)
+    for j in range(256):
+        if j < threshold:
+            table.append(0)
+        else:
+            table.append(1)
 
-        bim = lim.point(table, '1')
-        bim.save('./img/cut.png')
-        text = pytesseract.image_to_string(bim)
-        text = re.findall('\d+', text)
-        text = ''.join(text)
-        print(text)
-        return text
+    bim = lim.point(table, '1')
+    bim.save('./img/cut.png')
+    text = pytesseract.image_to_string(bim)
+    text = re.findall('\d+', text)
+    text = ''.join(text)
+    print(text)
+    return text
 
 
 def main():
